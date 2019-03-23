cfa92b776b5cb1e23dcd8399ab61aa39aaba1a30
Vinh Nguyen <vinh.nguyenxuan@2359media.com>
2019-03-23 21:45:07 +0700

    Structure project

---
+++ Clendar/App/AppDelegate.swift
@@ -0,0 +1,19 @@
+//
+//  AppDelegate.swift
+//  Clendar
+//
+//  Created by Vinh Nguyen on 23/3/19.
+//  Copyright © 2019 Vinh Nguyen. All rights reserved.
+//
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    var window: UIWindow?
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        return true
+    }
+}
