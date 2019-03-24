6db11d6f0b994fe317af6360f5b5751f5b3632b6
Vinh Nguyen <vinh.nguyenxuan@2359media.com>
2019-03-24 15:22:29 +0700

    Add bridging header

---
+++ Clendar/Resources/Bridging-Header.h
@@ -0,0 +1,15 @@
+//
+//  Bridging-Header.h.h
+//  Clendar
+//
+//  Created by Vinh Nguyen on 24/3/19.
+//  Copyright © 2019 Vinh Nguyen. All rights reserved.
+//
+
+#ifndef Bridging_Header_h_h
+#define Bridging_Header_h_h
+
+#import <Foundation/Foundation.h>
+#import <UIKit/UIKit.h>
+
+#endif /* Bridging_Header_h_h */
