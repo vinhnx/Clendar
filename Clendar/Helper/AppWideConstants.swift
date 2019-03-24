e2bd4d8279a93ce91a8ca0abf1619bfa257fcd4c
Vinh Nguyen <vinh.nguyenxuan@2359media.com>
2019-03-24 11:42:07 +0700

    Add app wide constant

---
+++ Clendar/Helper/AppWideConstants.swift
@@ -0,0 +1,14 @@
+//
+//  AppWideConstants.swift
+//  Clendar
+//
+//  Created by Vinh Nguyen on 24/3/19.
+//  Copyright © 2019 Vinh Nguyen. All rights reserved.
+//
+
+import Foundation
+import UIKit
+import CVCalendar
+
+// typealiasing
+typealias DateUpdateHandler = (CVDate) -> Void
