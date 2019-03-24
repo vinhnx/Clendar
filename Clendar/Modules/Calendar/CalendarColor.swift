9ba527c7867a0517c3aae9aad3c8d03803cb0a38
Vinh Nguyen <vinh.nguyenxuan@2359media.com>
2019-03-24 13:59:33 +0700

    Add color config

---
+++ Clendar/Modules/Calendar/CalendarColor.swift
@@ -0,0 +1,20 @@
+//
+//  CalendarColor.swift
+//  Clendar
+//
+//  Created by Vinh Nguyen on 24/3/19.
+//  Copyright © 2019 Vinh Nguyen. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+struct CalendarColorsConfig {
+    static let selectedText = UIColor.white
+    static let text = UIColor.black
+    static let textDisabled = UIColor.gray
+    static let selectionBackground = UIColor(red: 0.2, green: 0.2, blue: 1.0, alpha: 1.0)
+    static let sundayText = UIColor(red: 1.0, green: 0.2, blue: 0.2, alpha: 1.0)
+    static let sundayTextDisabled = UIColor(red: 1.0, green: 0.6, blue: 0.6, alpha: 1.0)
+    static let sundaySelectionBackground = sundayText
+}
