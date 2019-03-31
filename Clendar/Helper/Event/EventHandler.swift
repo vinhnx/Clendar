7f4bc487ab05a93e307b6bc2fde23e523f9d9533
Vinh Nguyen <vinh.nguyenxuan@2359media.com>
2019-03-31 11:23:23 +0700

    Add Event fetching handler

--- Clendar/Helper/Event/EventHandler.swift
+++ Clendar/Helper/Event/EventHandler.swift
@@ -9,23 +9,37 @@
 import EventKit
 import Foundation
 
+#warning("TODO: refactor")
+#warning("TODO: convert all callbacks to Result callbacks")
+#warning("TODO: use Promise, Async/Await for nested callbacks")
+
 final class EventHandler {
 
     // MARK: - Properties
 
     private var eventStore = EKEventStore()
 
+    // MARK: - Life cycle
+
+    static let shared = EventHandler()
+    private init() {} // This prevents others from using the default '()' initializer for this class.
+
     // MARK: - Flow
 
     func request(onAuthorized: VoidHandler?) {
         let status = EKEventStore.authorizationStatus(for: .event)
         switch status {
-        case .authorized: break
+        case .authorized:
+            break
+
         case .denied, .restricted:
             AlertManager.showSettingsAlert(message: "Please authorize \(calendarName) to create Calendar events")
 
         case .notDetermined:
             self.requestCreatingEventAccess(onAuthorized: onAuthorized)
+
+        @unknown default:
+            break
         }
     }
 
@@ -46,6 +60,30 @@ final class EventHandler {
                                          calendar: calendar,
                                          completion: onDone)
         }
+    }
+
+    // MARK: - Fetch Events
+
+    func fetchEventsForToday(completion: EventResultHandler? = nil) {
+        let today = Date().within24h
+        self.fetchEvents(startDate: today.startTime, endDate: today.endTime, completion: completion)
+    }
+
+    func fetchEvents(startDate: Date, endDate: Date, completion: EventResultHandler? = nil) {
+        let status = EKEventStore.authorizationStatus(for: .event)
+        guard status == .authorized else {
+            self.request { [weak self] in
+                self?.fetchEvents(startDate: startDate, endDate: endDate, completion: completion)
+            }
+
+            completion.flatMap { $0(.failure(.failedToAuthorizeEventPersmissson)) }
+            return
+        }
+
+        let calendars = self.eventStore.calendars(for: .event)
+        let predicate = self.eventStore.predicateForEvents(withStart: startDate, end: endDate, calendars: calendars)
+        let events = self.eventStore.events(matching: predicate)
+        completion.flatMap { $0(.success(events)) }
     }
 
     // MARK: - Private
