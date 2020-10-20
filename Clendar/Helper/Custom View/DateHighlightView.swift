417f9cc169ea9a26e5723e544914e8a634e5c067
Vinh Nguyen <vinhnguyen2308@gmail.com>
2020-10-20 21:50:16 +0700

    Use Foundation DateFormatter with Chinese calendar identifier to format lunar date

--- Clendar/Helper/Custom View/DateHighlightView.swift
+++ Clendar/Helper/Custom View/DateHighlightView.swift
@@ -10,30 +10,14 @@ import Foundation
 import CVCalendar
 
 class DateHighlightView: UIStackView {
-    static func viewForDayView(_ dayView: DayView) -> DateHighlightView? {
+    static func viewForDayView(_ dayView: DayView, isOut: Bool) -> UIView? {
         guard let date = dayView.date.convertedDate() else { return nil }
-        let size: CGFloat = 5.0
-        let view = DateHighlightView(frame: CGRect(x: dayView.frame.origin.x,
-                                                   y: dayView.frame.size.height,
-                                                   width: dayView.frame.size.width * 0.9,
-                                                   height: size))
-        view.axis = .horizontal
-        view.distribution = .fillEqually
-        EventHandler.shared.fetchEvents(for: date) { (result) in
-            switch result {
-            case .success(let events):
-                for event in events {
-                    let colorView = UIView(frame: CGRect(x: 0, y: 0, width: size, height: size))
-                    colorView.backgroundColor = UIColor.init(cgColor: event.calendar.cgColor)
-                    colorView.applyCircle()
-                    view.addArrangedSubview(colorView)
-                }
-
-            case .failure:
-                break
-            }
-        }
-
-        return view
+        let lunarDate = DateFormatter.lunarDateString(forDate: date)
+        let label = UILabel(frame: CGRect(x: dayView.frame.origin.x, y: dayView.frame.size.height * 0.7,
+                                          width: dayView.frame.size.width, height: 10))
+        label.textColor = isOut ? .systemGray3 : .systemGray
+        label.font = FontConfig.regularFontWithSize(11)
+        label.text = lunarDate
+        return label
     }
 }
