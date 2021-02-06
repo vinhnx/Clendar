150499b67d00bb044fceab071bffc0c668b318e7
Vinh Nguyen <vinhnguyen2308@gmail.com>
2021-02-07 01:24:42 +0700

    Fix swiftlint

--- Clendar/ClendarApp.swift
+++ Clendar/ClendarApp.swift
@@ -122,7 +122,7 @@ extension ClendarApp {
         }
 
         // handle App Store transaction
-        SwiftyStoreKit.shouldAddStorePaymentHandler = { payment, product in
+        SwiftyStoreKit.shouldAddStorePaymentHandler = { _, product in
             // return true if the content can be delivered by your app
             // return false otherwise
             CosumablePurchaseProductIdentifier.allCases.compactMap { $0.rawValue }.contains(product.productIdentifier)
